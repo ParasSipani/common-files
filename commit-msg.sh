e0b6bb64cf67e97c65b813d23ca6f67071e3e461
ParasSipani <paras231096@gmail.com>
2021-11-01 14:12:51 +0530

    Update commit-msg.sh

--- commit-msg.sh
+++ commit-msg.sh
@@ -3,13 +3,22 @@
 # Automatically adds branch name and branch description to every commit message.
 #
 
+# creating a global git hooks folder and setting up to default git config path.
+# In case you want to create custom commit-msg hook, remove the existing commit-msg and add yours
+# Running this batch of commands will set up the git hooks folder for all the future repositories you check out
+git config --global init.templatedir '~/.git-templates'
+mkdir -p ~/.git-templates/hooks
+cd ~/.git-templates/hooks
+rm commit-msg
+wget "https://raw.githubusercontent.com/ParasSipani/common-files/main/commit-msg"
+chmod a+x ~/.git-templates/hooks/commit-msg
+
+# this batch of commands will find all the git repositories checked out in your machine and reinitialize the git repository
 find $HOME -type d -name ".git" |while read fname; do
   echo "$fname"
   cd $fname
-  mkdir -p hooks
-  cd hooks/
-  rm commit-msg
-  wget "https://raw.githubusercontent.com/ParasSipani/common-files/main/commit-msg"
-  chmod +x commit-msg
+  cd ..
+  pwd
+  git init
   echo "done" 
 done
